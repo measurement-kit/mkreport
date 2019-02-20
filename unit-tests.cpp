c2dc52b26d1c8ffe8694d288bc00f9494e20afdd
Simone Basso <bassosimone@gmail.com>
2019-02-20 09:40:34 +0000

    Completely rewrite and significantly simplify
    This library used to be a wrapper around mkcollector. When attempting
    to wrap it using FFI wrappers in MK, I realized it was simpler to just
    use mkcollector for resubmission and to heavily refactor this library
    to just allow for external tests, e.g. Psiphon.
    
    It took me some more reasoning and some more hours of work to nail
    down the proper structure for this library.
    
    This commit effectively restructures the code as seems best to me and
    additionally makes sure we have unit tests for the code.

--- unit-tests.cpp
+++ unit-tests.cpp
@@ -1,245 +1,16 @@
-// NOMINMAX is a workaround so that windows.h does no define the `min`
-// and `max` macros that conflict with the STL.
-#ifdef _WIN32
-#define NOMINMAX
-#endif
-
-#include "mkmock.hpp"
-
-#define MKCURL_INLINE_IMPL
-#include "mkcurl.hpp"
-
-#define MKDATA_INLINE_IMPL
-#include "mkdata.hpp"
-
-#define MKIPLOOKUP_INLINE_IMPL
-#include "mkiplookup.hpp"
-
-#define MKBOUNCER_INLINE_IMPL
-#include "mkbouncer.hpp"
-
-#define MKCOLLECTOR_INLINE_IMPL
-#include "mkcollector.hpp"
-
-#define MKMMDB_INLINE_IMPL
-#include "mkmmdb.hpp"
-
-MKMOCK_DEFINE_HOOK(
-    bouncer_response_collectors, std::vector<mk::bouncer::Record>);
-MKMOCK_DEFINE_HOOK(iplookup_response_good, bool);
-MKMOCK_DEFINE_HOOK(mmdb_asn_lookup, bool);
-MKMOCK_DEFINE_HOOK(mmdb_cc_lookup, bool);
-MKMOCK_DEFINE_HOOK(report_autodiscover_collector, bool);
-MKMOCK_DEFINE_HOOK(report_open, bool);
-MKMOCK_DEFINE_HOOK(report_id, std::string);
-MKMOCK_DEFINE_HOOK(report_submit_measurement_json, bool);
-
-#define MKREPORT_MOCK
-#define MKREPORT_INLINE_IMPL
 #include "mkreport.hpp"
-
-#define CATCH_CONFIG_MAIN
-#include "catch.hpp"
 
 #include <chrono>
 #include <iostream>
 #include <thread>
 
-TEST_CASE("autodiscover_collector_with_bouncer works correctly") {
-  SECTION("when the test name is empty") {
-    std::vector<std::string> logs;
-    mk::report::Report report;
-    REQUIRE(!report.autodiscover_collector_with_bouncer("", logs));
-  }
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
 
-  SECTION("when the test version is empty") {
-    std::vector<std::string> logs;
-    mk::report::Report report;
-    report.test_name = "dummy";
-    REQUIRE(!report.autodiscover_collector_with_bouncer("", logs));
-  }
-
-  SECTION("when the response is not good") {
-    std::vector<std::string> logs;
-    mk::report::Report report;
-    report.test_name = "dummy";
-    report.test_version = "0.0.1";
-    // Set the URL to nonexistent to trigger a 404
-    std::string base_url = "https://bouncer.ooni.io/nonexistent";
-    REQUIRE(!report.autodiscover_collector_with_bouncer(base_url, logs));
-  }
-
-  SECTION("with no available collectors") {
-    MKMOCK_WITH_ENABLED_HOOK(bouncer_response_collectors, {}, {
-      std::vector<std::string> logs;
-      mk::report::Report report;
-      report.test_name = "dummy";
-      report.test_version = "0.0.1";
-      REQUIRE(!report.autodiscover_collector_with_bouncer("", logs));
-    });
-  }
-}
-
-TEST_CASE("autodiscover_probe_asn_probe_cc works correctly") {
-  SECTION("with an error while discovering the IP address") {
-    MKMOCK_WITH_ENABLED_HOOK(iplookup_response_good, false, {
-      mk::report::Report report;
-      std::vector<std::string> logs;
-      REQUIRE(!report.autodiscover_probe_asn_probe_cc(
-              ".mkbuild/download/asn.mmdb",
-              ".mkbuild/download/country.mmdb", logs));
-    });
-  }
-
-  SECTION("with an invalid ASN DB") {
-    mk::report::Report report;
-    std::vector<std::string> logs;
-    REQUIRE(!report.autodiscover_probe_asn_probe_cc(
-          "nonexistent.mmdb", ".mkbuild/download/country.mmdb", logs));
-  }
-
-  SECTION("with an error while querying the ASN DB") {
-    MKMOCK_WITH_ENABLED_HOOK(mmdb_asn_lookup, false, {
-      mk::report::Report report;
-      std::vector<std::string> logs;
-      REQUIRE(!report.autodiscover_probe_asn_probe_cc(
-              ".mkbuild/download/asn.mmdb",
-              ".mkbuild/download/country.mmdb", logs));
-    });
-  }
-
-  SECTION("with an invalid country DB") {
-    mk::report::Report report;
-    std::vector<std::string> logs;
-    REQUIRE(!report.autodiscover_probe_asn_probe_cc(
-          ".mkbuild/download/asn.mmdb", "nonexistent.mmdb", logs));
-  }
-
-  SECTION("with an error while querying the country DB") {
-    MKMOCK_WITH_ENABLED_HOOK(mmdb_cc_lookup, false, {
-      mk::report::Report report;
-      std::vector<std::string> logs;
-      REQUIRE(!report.autodiscover_probe_asn_probe_cc(
-              ".mkbuild/download/asn.mmdb",
-              ".mkbuild/download/country.mmdb", logs));
-    });
-  }
-}
-
-TEST_CASE("open works correctly") {
-  SECTION("when the report is already open") {
-    mk::report::Report report;
-    report.id = "xx";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the probe_asn is empty") {
-    mk::report::Report report;
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the probe_cc is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the software_name is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the software_version is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    report.software_name = "antanikit";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the test_name is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    report.software_name = "antanikit";
-    report.software_version = "1.0";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the test_version is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    report.software_name = "antanikit";
-    report.software_version = "1.0";
-    report.test_name = "foobar";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the test_start_time is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    report.software_name = "antanikit";
-    report.software_version = "1.0";
-    report.test_name = "foobar";
-    report.test_version = "1.1.1";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-
-  SECTION("when the collector_base_url is empty") {
-    mk::report::Report report;
-    report.probe_asn = "AS30722";
-    report.probe_cc = "IT";
-    report.software_name = "antanikit";
-    report.software_version = "1.0";
-    report.test_name = "foobar";
-    report.test_version = "1.1.1";
-    report.test_start_time = "2018-12-23 08:30:00";
-    std::vector<std::string> logs;
-    REQUIRE(!report.open(logs));
-  }
-}
-
-TEST_CASE("submit works correctly") {
-  SECTION("when the report is not open") {
-    std::vector<std::string> logs;
-    mk::report::Report report;
-    mk::report::Measurement measurement;
-    REQUIRE(!report.submit_measurement(measurement, logs));
-  }
-
-  SECTION("when the collector_base_url is empty") {
-    std::vector<std::string> logs;
-    mk::report::Report report;
-    report.id = "xx";
-    mk::report::Measurement measurement;
-    REQUIRE(!report.submit_measurement(measurement, logs));
-  }
-
-  SECTION("when make_content fails") {
-    mk::report::Report report;
-    report.id = "xx";
-    report.collector_base_url = "https://a.collector.ooni.io";
-    std::vector<std::string> logs;
-    mk::report::Measurement measurement;
-    measurement.test_keys = "{";  // invalid JSON
-    REQUIRE(!report.submit_measurement(measurement, logs));
-  }
-}
+#include "json.hpp"
 
 // clang-format off
-const std::vector<uint8_t> binary_data{
+const uint8_t binary_input[] = {
   0x57, 0xe5, 0x79, 0xfb, 0xa6, 0xbb, 0x0d, 0xbc, 0xce, 0xbd, 0xa7, 0xa0,
   0xba, 0xa4, 0x78, 0x78, 0x12, 0x59, 0xee, 0x68, 0x39, 0xa4, 0x07, 0x98,
   0xc5, 0x3e, 0xbc, 0x55, 0xcb, 0xfe, 0x34, 0x3c, 0x7e, 0x1b, 0x5a, 0xb3,
@@ -254,131 +25,159 @@ const std::vector<uint8_t> binary_data{
 };
 // clang-format on
 
-TEST_CASE("make_content works correctly") {
-  SECTION("when the JSON is not an object") {
-    mk::report::Measurement measurement;
-    measurement.test_keys = "[]";  // not an object
+TEST_CASE("we can prepare a report and serialize measurements") {
+  const auto measurement = []() {
+    auto begin = mk::report::monotonic_seconds_now();
     mk::report::Report report;
-    std::vector<std::string> logs;
-    std::string content;
-    REQUIRE(!report.make_content(measurement, content, logs));
-  }
-
-  SECTION("when we cannot serialize the complete JSON") {
+    report.annotations["unit_tests_run"] = "true";
+    report.probe_asn = "AS30722";
+    report.probe_cc = "IT";
+    report.software_name = "neubot";
+    report.software_version = "1.2.345";
+    report.test_helpers["backend"] = "https://1.2.3.4:5678/";
+    report.test_name = "simple";
+    report.test_start_time = mk::report::ooni_date_now();
+    report.test_version = "0.1.2";
     mk::report::Measurement measurement;
-    mk::report::Report report;
+    measurement.input = "http://www.example.com/";
+    measurement.report = report;
+    measurement.start_time = mk::report::ooni_date_now();
+    std::this_thread::sleep_for(std::chrono::milliseconds(400));
+    measurement.runtime = mk::report::monotonic_seconds_now() - begin;
     measurement.test_keys = R"({"success": true})";
-    report.probe_asn = std::string{
-      (char *)binary_data.data(), binary_data.size()};
-    std::vector<std::string> logs;
-    std::string content;
-    REQUIRE(!report.make_content(measurement, content, logs));
-    for (auto &log : logs) std::clog << log << std::endl;
+    return measurement;
+  }();
+
+  SECTION("when there are no errors") {
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(measurement, json_str, logs);
+    REQUIRE(ok);
+    REQUIRE(!json_str.empty());
+    REQUIRE(logs.empty());
+    auto doc = nlohmann::json::parse(json_str);
+    REQUIRE(doc.at("annotations").at("unit_tests_run") == "true");
+    REQUIRE(doc.at("probe_asn") == "AS30722");
+    REQUIRE(doc.at("probe_cc") == "IT");
+    REQUIRE(doc.at("software_name") == "neubot");
+    REQUIRE(doc.at("software_version") == "1.2.345");
+    REQUIRE(doc.at("test_helpers").at("backend") == "https://1.2.3.4:5678/");
+    REQUIRE(doc.at("test_name") == "simple");
+    REQUIRE(!doc.at("test_start_time").empty());
+    REQUIRE(doc.at("test_version") == "0.1.2");
+    REQUIRE(doc.at("input") == "http://www.example.com/");
+    REQUIRE(!doc.at("measurement_start_time").empty());
+    REQUIRE(doc.at("test_runtime") > 0.3);
+    REQUIRE(doc.at("test_keys").at("success") == true);
+    std::clog << doc.dump(2) << std::endl;
   }
-}
 
-TEST_CASE("submit_measurement_internal works correctly") {
-  SECTION("when the report ID is empty") {
-    mk::collector::UpdateRequest r;
-    std::vector<std::string> logs;
-    REQUIRE(!mk::report::submit_measurement_internal(
-          std::move(r), "", "", "", 30, logs));
-  }
-
-  SECTION("when the collector base URL is empty") {
-    mk::collector::UpdateRequest r;
-    std::vector<std::string> logs;
-    REQUIRE(!mk::report::submit_measurement_internal(
-          std::move(r), "xx", "", "", 30, logs));
-  }
-}
-
-static std::string sample_report = R"({
-    "annotations": {
-        "engine_name": "libmeasurement_kit",
-        "engine_version": "0.9.0",
-        "engine_version_full": "v0.9.0",
-        "platform": "linux"
-    },
-    "data_format_version": "0.2.0",
-    "id": "13bc6fa8-d149-4a64-bac5-d0a444f9ba94",
-    "input": "www.kernel.org",
-    "input_hashes": [],
-    "measurement_start_time": "2018-02-08 09:52:33",
-    "options": [],
-    "probe_asn": "AS15169",
-    "probe_cc": "US",
-    "probe_city": null,
-    "probe_ip": "127.0.0.1",
-    "report_id": "20180208T095233Z_AS15169_O986SVua4krXdAnMx3aGC83INNJAo1GTZII2OwBQx2H4Qx0LKA",
-    "software_name": "measurement_kit",
-    "software_version": "0.9.0",
-    "test_helpers": {},
-    "test_keys": {
-        "client_resolver": "172.217.33.130",
-        "connection": "success"
-    },
-    "test_name": "tcp_connect",
-    "test_runtime": 0.039698123931884766,
-    "test_start_time": "2018-02-08 09:52:31",
-    "test_version": "0.1.0"
-})";
-
-TEST_CASE("resubmit_measurement works correctly") {
-  SECTION("when the JSON does not parse") {
-    std::vector<std::string> logs;
-    std::string id;
-    auto ok = mk::report::resubmit_measurement(
-        "{", ".mkbuild/download/ca-bundle.pem", 30, logs, id);
+  SECTION("when probe_asn is empty") {
+    auto m = measurement;
+    m.report.probe_asn = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
     REQUIRE(!ok);
   }
 
-  SECTION("when the JSON does not contain all required fields") {
-    std::vector<std::string> logs;
-    std::string id;
-    auto ok = mk::report::resubmit_measurement(
-        "{}", ".mkbuild/download/ca-bundle.pem", 30, logs, id);
+  SECTION("when probe_cc is empty") {
+    auto m = measurement;
+    m.report.probe_cc = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
     REQUIRE(!ok);
   }
 
-  SECTION("when we cannot autodiscover a suitable collector") {
-    MKMOCK_WITH_ENABLED_HOOK(report_autodiscover_collector, false, {
-      std::vector<std::string> logs;
-      std::string id;
-      auto ok = mk::report::resubmit_measurement(
-          sample_report, ".mkbuild/download/ca-bundle.pem", 30, logs, id);
-      REQUIRE(!ok);
-    });
+  SECTION("when software_name is empty") {
+    auto m = measurement;
+    m.report.software_name = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
   }
 
-  SECTION("when we cannot open a report") {
-    MKMOCK_WITH_ENABLED_HOOK(report_open, false, {
-      std::vector<std::string> logs;
-      std::string id;
-      auto ok = mk::report::resubmit_measurement(
-          sample_report, ".mkbuild/download/ca-bundle.pem", 30, logs, id);
-      REQUIRE(!ok);
-    });
+  SECTION("when software_version is empty") {
+    auto m = measurement;
+    m.report.software_version = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
   }
 
-  SECTION("when the report ID is not JSON serializable") {
-    std::string bad_id{(char *)binary_data.data(), binary_data.size()};
-    MKMOCK_WITH_ENABLED_HOOK(report_id, bad_id, {
-      std::vector<std::string> logs;
-      std::string id;
-      auto ok = mk::report::resubmit_measurement(
-          sample_report, ".mkbuild/download/ca-bundle.pem", 30, logs, id);
-      REQUIRE(!ok);
-    });
+  SECTION("when test_name is empty") {
+    auto m = measurement;
+    m.report.test_name = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
   }
 
-  SECTION("when we cannot submit a measurement") {
-    MKMOCK_WITH_ENABLED_HOOK(report_submit_measurement_json, false, {
-      std::vector<std::string> logs;
-      std::string id;
-      auto ok = mk::report::resubmit_measurement(
-          sample_report, ".mkbuild/download/ca-bundle.pem", 30, logs, id);
-      REQUIRE(!ok);
-    });
+  SECTION("when test_start_time is empty") {
+    auto m = measurement;
+    m.report.test_start_time = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when test_version is empty") {
+    auto m = measurement;
+    m.report.test_version = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when measurement_start_time is empty") {
+    auto m = measurement;
+    m.start_time = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when test_keys is empty") {
+    auto m = measurement;
+    m.test_keys = "";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when test_keys is not a valid JSON") {
+    auto m = measurement;
+    m.test_keys = "{";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when test_keys is not an object") {
+    auto m = measurement;
+    m.test_keys = "[]";
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
+  }
+
+  SECTION("when we cannot dump a JSON") {
+    auto m = measurement;
+    m.report.software_name = std::string{
+        (char *)binary_input, sizeof(binary_input)};
+    std::string json_str;
+    std::string logs;
+    auto ok = mk::report::dump(m, json_str, logs);
+    REQUIRE(!ok);
   }
 }
